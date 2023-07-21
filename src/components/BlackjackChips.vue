1f9c1767f272ea7fd7ba2610b5cfaf3a64048c73
David Benner <davidbenner@davids-mbp.lan>
2023-07-21 16:34:05 -0400

    Add BlackjackChips file with basic code

---
+++ src/components/BlackjackChips.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="chips">
+    <p>Chips: {{ chips }}</p>
+    <p>Bet: {{ bet }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["chips", "bet"],
+};
+</script>
