41008f4d52a192e33d8ad34762807e0f2d79d96d
David Benner <davidbenner@davids-mbp.lan>
2023-07-21 16:57:56 -0400

    Add GameButtons, Vue component for the game control buttons

---
+++ src/components/GameButtons.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="game-buttons">
+    <button @click="startGame" v-if="!gameStarted">Start Game</button>
+    <button @click="hit" v-if="gameStarted && !gameOver">Hit</button>
+    <button @click="stand" v-if="gameStarted && !gameOver">Stand</button>
+    <button @click="restartGame" v-if="gameOver">Restart Game</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    "gameStarted",
+    "gameOver",
+    "startGame",
+    "hit",
+    "stand",
+    "restartGame",
+  ],
+};
+</script>
+
+<style>
+/* Add your CSS styling here for the game buttons */
+</style>
