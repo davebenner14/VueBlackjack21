953889d80f7dc1ad63d923c87a957782be9326f2
David Benner <davidbenner@davids-mbp.lan>
2023-07-21 16:58:55 -0400

    Add GameStatus, Vue component for the game status message

---
+++ src/components/GameStatus.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="game-status">
+    <p v-if="message">{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["message"],
+};
+</script>
+
+<style>
+/* Add your CSS styling here for the game status */
+</style>
