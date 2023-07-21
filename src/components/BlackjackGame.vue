1d16e4de3aaece8b2c265698d4c57118796463f9
David Benner <davidbenner@davids-mbp.lan>
2023-07-21 16:53:00 -0400

    Update BlackjackGame with calc hands method

--- src/components/BlackjackGame.vue
+++ src/components/BlackjackGame.vue
@@ -4,10 +4,10 @@
     <h1 class="game-title">Vue Blackjack 21</h1>
 
     <!-- Dealer's Hand -->
-    <Hand :cards="dealerHand" title="Dealer's Hand" />
+    <BlackjackHand :cards="dealerHand" title="Dealer's Hand" />
 
     <!-- Player's Hand -->
-    <Hand :cards="playerHand" title="Player's Hand" />
+    <BlackjackHand :cards="playerHand" title="Player's Hand" />
 
     <!-- Buttons and Game Status -->
     <div class="game-status">
@@ -18,7 +18,7 @@
     </div>
 
     <!-- Chips -->
-    <Chips :chips="chips" :bet="bet" />
+    <BlackjackChips :chips="chips" :bet="bet" />
 
     <!-- Restart Game Button -->
     <button @click="restartGame" v-if="gameOver">Restart Game</button>
@@ -26,8 +26,8 @@
 </template>
 
 <script>
-import Hand from "./BlackjackHand.vue"; // Hand component handles the display of player's and dealer's hands
-import Chips from "./Chips.vue"; // Chips component handles the display and management of chips
+import BlackjackHand from "./BlackjackHand.vue";
+import BlackjackChips from "./BlackjackChips.vue";
 
 export default {
   data() {
@@ -43,11 +43,100 @@ export default {
     };
   },
   methods: {
-    // Game logic methods (startGame, hit, stand, etc.)
+    initializeGame() {
+      this.gameStarted = true;
+      this.createAndShuffleDeck();
+      this.dealerHand = [this.drawCard(), this.drawCard()];
+      this.playerHand = [this.drawCard(), this.drawCard()];
+    },
+    hit() {
+      this.playerHand.push(this.drawCard());
+      if (this.calculateHandValue(this.playerHand) > 21) {
+        this.message = "Player Busted!";
+        this.gameOver = true;
+      }
+    },
+    stand() {
+      while (this.calculateHandValue(this.dealerHand) < 17) {
+        this.dealerHand.push(this.drawCard());
+      }
+      this.gameOver =
+        this.calculateHandValue(this.dealerHand) > 21 ? true : false;
+      this.message = this.gameOver ? "Dealer Busted!" : "";
+      if (!this.gameOver) {
+        this.resolveGame();
+      }
+    },
+    restartGame() {
+      this.gameOver = false;
+      this.gameStarted = false;
+      this.message = "";
+      this.dealerHand = [];
+      this.playerHand = [];
+    },
+    createAndShuffleDeck() {
+      const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+      const values = [
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "J",
+        "Q",
+        "K",
+        "A",
+      ];
+      this.deck = [];
+      for (let suit of suits) {
+        for (let value of values) {
+          this.deck.push({ suit: suit, value: value });
+        }
+      }
+      for (let i = this.deck.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [this.deck[i], this.deck[j]] = [this.deck[j], this.deck[i]];
+      }
+    },
+    drawCard() {
+      return this.deck.pop();
+    },
+    calculateHandValue(hand) {
+      let value = 0;
+      let aces = 0;
+
+      hand.forEach((card) => {
+        if (["J", "Q", "K"].includes(card.value)) {
+          value += 10;
+        } else if (card.value === "A") {
+          value += 11;
+          aces += 1; // Count the number of aces
+        } else {
+          value += parseInt(card.value);
+        }
+      });
+
+      // If value is over 21 and there are aces in the hand, start converting aces from 11 to 1
+      while (value > 21 && aces > 0) {
+        value -= 10; // Convert an Ace from 11 to 1
+        aces -= 1; // We've used up one Ace
+      }
+
+      return value;
+    },
+
+    resolveGame() {
+      // Logic for comparing playerHand and dealerHand to determine the winner
+    },
   },
+
   components: {
-    Hand,
-    Chips,
+    BlackjackHand,
+    BlackjackChips,
   },
 };
 </script>
---
+++ src/components/BlackjackHand.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+    <div class="hand">
+      <div v-for="card in cards" :key="card">
+        {{ card.suit }} - {{ card.value }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cards: Array,
+    title: String,
+  },
+};
+</script>
+
+<style>
+/* Add CSS styling for the BlackjackHand component here */
+</style>
