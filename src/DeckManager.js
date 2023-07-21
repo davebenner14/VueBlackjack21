0fb523d55fcaeb9370d6311f2edc7191ee40024d
David Benner <davidbenner@davids-mbp.lan>
2023-07-21 16:56:01 -0400

    Add DeckManager

---
+++ src/DeckManager.js
@@ -0,0 +1,32 @@
+export function createAndShuffleDeck() {
+  const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+  const values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A",
+  ];
+  let deck = [];
+
+  for (let suit of suits) {
+    for (let value of values) {
+      deck.push({ suit: suit, value: value });
+    }
+  }
+
+  for (let i = deck.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [deck[i], deck[j]] = [deck[j], deck[i]];
+  }
+
+  return deck;
+}
