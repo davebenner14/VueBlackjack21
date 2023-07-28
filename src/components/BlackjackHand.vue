924780df92eb5d496030afd9c4ed16d8f545f753
David Benner <davidbenner@davids-mbp.lan>
2023-07-28 16:10:17 -0400

    Update Card styling

--- src/components/BlackjackHand.vue
+++ src/components/BlackjackHand.vue
@@ -4,7 +4,10 @@
     <h2 v-else>{{ title }} (Total: {{ totalValue }})</h2>
     <ul class="hand">
       <li v-for="(card, index) in cards" :key="index">
-        <Card :card="card" v-if="!isDealer || index !== 1 || dealerTurn" />
+        <PlayingCard
+          :card="card"
+          v-if="!isDealer || index !== 1 || dealerTurn"
+        />
         <div class="card back" v-else></div>
       </li>
     </ul>
@@ -13,11 +16,11 @@
 
 <script>
 import { calculateHandValue } from "../utils/CardUtils";
-import Card from "./PlayingCard.vue";
+import PlayingCard from "./PlayingCard.vue";
 
 export default {
   components: {
-    Card,
+    PlayingCard,
   },
   props: ["cards", "title", "isDealer", "dealerTurn"],
   computed: {
@@ -33,4 +36,44 @@ export default {
 };
 </script>
 
-<style src="../styles/Card.css"></style>
+<style scoped>
+.hand {
+  display: flex;
+  gap: 10px;
+  list-style: none;
+  padding: 0;
+}
+
+.card.back {
+  background: linear-gradient(160deg, #2952a3, #b04e0f);
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 60px;
+  height: 90px;
+  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.3);
+  border-radius: 5px;
+  font-size: 20px;
+  padding: 5px;
+  position: relative;
+}
+
+.card.back:before {
+  content: "";
+  position: absolute;
+  width: 40px;
+  height: 40px;
+  background: linear-gradient(
+    45deg,
+    #fff0,
+    #fff0 5px,
+    #ffffff55 5px,
+    #ffffff55 10px,
+    #fff0 10px
+  );
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%) rotate(45deg);
+}
+</style>
